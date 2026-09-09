7fdc4f36b73b04c30c1561d8e8c9c574d561717a
ReiFxN <reifannst55@gmail.com>
2026-09-09 15:18:14 +0700

    Pertemuan 1: Array & Dynamic Array

---
+++ Pertemuan(1)/Pertemuan1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Masukin nilai: ";
+    cin >> n;
+
+    int* arr = new int[n]; //dynamic array
+
+    cout << "Masukin " << n << " angka :\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Isi Array : ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+    return 0;
+}
